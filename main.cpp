2a6d616cb724a8ec96f7ce10b144fa3c5dd01e3f
YuunqiLiu <liuyuunqi@163.com>
2023-12-15 09:30:26 +0800

    update.

--- main.cpp
+++ main.cpp
@@ -2,12 +2,10 @@
 
 #include <iostream>
 #include <bitset>
-
 #include <random>
 #include <limits>
 
 using namespace std;
-
 
 
 class Fp32{
@@ -15,19 +13,13 @@ class Fp32{
     public:
         bool     sign;
         uint32_t exponent;
-        uint64_t fraction;
+        uint64_t mantissa;
 
-    void display(){
-        cout << sign << endl;
-        cout << exponent << endl;
-        cout << fraction << endl;
-
-    }
 
     Fp32() {
         sign = 0;
         exponent = 0;
-        fraction = 0;
+        mantissa = 0;
     }
 
     // constructor, convert a float to Fp32 type
@@ -35,7 +27,7 @@ class Fp32{
         uint32_t bits = *(uint32_t*)&x; // get the binary representation of the float
         sign     = bits >> 31;          // get the sign bit
         exponent = (bits >> 23) & 0xff; // get the exponent bit
-        fraction = bits & 0x7fffff;     // get the fraction bit
+        mantissa = bits & 0x7fffff;     // get the mantissa bit
     }
 
 
@@ -44,7 +36,7 @@ class Fp32{
         uint32_t bits = 0;          // initialize the binary representation of the float
         bits |= sign << 31;         // set the sign bit
         bits |= exponent << 23;     // set the exponent bit
-        bits |= fraction;           // set the fraction bit
+        bits |= mantissa;           // set the mantissa bit
         float x = *(float*)&bits;   // get the float value
         return x;
     }
@@ -53,15 +45,24 @@ class Fp32{
         uint32_t bits = 0;          // initialize the binary representation of the float
         bits |= sign << 31;         // set the sign bit
         bits |= exponent << 23;     // set the exponent bit
-        bits |= fraction;           // set the fraction bit
+        bits |= mantissa;           // set the mantissa bit
         return bits;
     }
+
+    uint32_t to_uint32() {
+        uint32_t bits = 0;          // initialize the binary representation of the float
+        bits |= sign << 31;         // set the sign bit
+        bits |= exponent << 23;     // set the exponent bit
+        bits |= mantissa;           // set the mantissa bit
+        return bits;
+    }
+
 
     void print() {
         printf("value      : %12f\n", (float)*this);
         printf(" - sign    : %12ld\n",sign);
         printf(" - exponent: %12ld,%64lb, 2^(%d)\n",exponent,exponent,exponent-127);
-        printf(" - fraction: %12ld,%64lb, %f\n",fraction,fraction+ (1 << 23),(float)(fraction + (1 << 23))/(1<<23));
+        printf(" - mantissa: %12ld,%64lb, %f\n",mantissa,mantissa+ (1 << 23),(float)(mantissa + (1 << 23))/(1<<23));
         printf("\n");
     }
 
@@ -73,33 +74,33 @@ class Fp32{
         result.sign = sign ^ rhs.sign; // calculate the sign of the result
         result.exponent = exponent + rhs.exponent - 127; // calculate the exponent of the result
 
-        result.fraction = (fraction | 0x800000) * (rhs.fraction | 0x800000); // calculate the significand of the result
+        result.mantissa = (mantissa | 0x800000) * (rhs.mantissa | 0x800000); // calculate the significand of the result
 
-        //printf("%lb\n",result.fraction);
+        //printf("%lb\n",result.mantissa);
         //printf("%lb\n",0x800000000000);
         //printf("?????????\n");
-        if (result.fraction & 0x800000000000) { // if the significand of the result exceeds 24 bits
-            //printf("%lb\n",result.fraction);
-            result.fraction >>= 1; // shift right by one bit
-            //printf("%lb\n",result.fraction);
+        if (result.mantissa & 0x800000000000) { // if the significand of the result exceeds 24 bits
+            //printf("%lb\n",result.mantissa);
+            result.mantissa >>= 1; // shift right by one bit
+            //printf("%lb\n",result.mantissa);
             result.exponent++; // increment the exponent
         }
         if (result.exponent > 254) { // if the exponent of the result exceeds the maximum value
             result.exponent = 255; // set the exponent to infinity
-            result.fraction = 0; // set the significand to 0
+            result.mantissa = 0; // set the significand to 0
         } else if (result.exponent < 1) { // if the exponent of the result is less than the minimum value
             result.exponent = 0; // set the exponent to 0
-            result.fraction = 0; // set the significand to 0
+            result.mantissa = 0; // set the significand to 0
         } else { // if the exponent of the result is within the valid range
-            result.fraction = result.fraction >> 23;
-            result.fraction &= 0x7fffff; // remove the hidden 1
-            uint32_t g = result.fraction & 0x40; // get the guard bit
-            uint32_t r = result.fraction & 0x20; // get the round bit
-            uint32_t s = result.fraction & 0x1f; // get the sticky bit
+            result.mantissa = result.mantissa >> 23;
+            result.mantissa &= 0x7fffff; // remove the hidden 1
+            uint32_t g = result.mantissa & 0x40; // get the guard bit
+            uint32_t r = result.mantissa & 0x20; // get the round bit
+            uint32_t s = result.mantissa & 0x1f; // get the sticky bit
             if (g && (r || s)) { // if rounding is needed
-                result.fraction++; // increment the least significant bit
-                if (result.fraction & 0x800000) { // if rounding causes overflow
-                    result.fraction >>= 1; // shift right by one bit
+                result.mantissa++; // increment the least significant bit
+                if (result.mantissa & 0x800000) { // if rounding causes overflow
+                    result.mantissa >>= 1; // shift right by one bit
                     result.exponent++; // increment the exponent
                 }
             }
@@ -118,52 +119,63 @@ class Fp32{
 };
 
 
+
+class Tester{
+
+    public:
+
+    float get_rand_float() {
+        float res;
+
+        std::random_device rd;
+        std::default_random_engine eng(rd());
+        std::uniform_int_distribution<uint32_t> distr(numeric_limits<uint32_t>::min(),numeric_limits<uint32_t>::max());
+
+        uint32_t tmp1 = distr(eng);
+        res = *(float*)&tmp1;
+        return res;
+    }
+
+    int test_once() {
+
+        float num1 = get_rand_float();
+        float num2 = get_rand_float();
+        float res = num1 * num2;
+
+        Fp32 hw_num1(num1);
+        Fp32 hw_num2(num2);
+        Fp32 hw_res;
+        hw_res = hw_num1 * hw_num2;
+
+        uint32_t tmp1 = *(uint32_t*)&res;
+        uint32_t tmp2 = hw_res.to_uint32();
+
+        if (tmp1 != tmp2) {
+            printf("%d\n",tmp1);
+            printf("%d\n",tmp2);
+            printf("%b\n",tmp1);
+            printf("%b\n",tmp2);
+            printf("?????\n");
+        }
+
+    }
+
+    int test_equal() {
+        for (int n = 0; n < 10; n++) {
+            test_once();
+        }
+    }
+
+};
+
+
+
 int main(int argc, char **argv)
 {
-
-    float num1,num2,res;
-
-    num1 = 1.1f;
-    num2 = 1.3f;
-
-    res = num1*num2;
-    
-    
-    Fp32 hw_num1(num1);
-    Fp32 hw_num2(num2);
-    Fp32 hw_res;
-
-
-    hw_res = hw_num1 * hw_num2;
-
-    cout << bitset<32>(*(uint32_t*)&res) << endl;
-    cout << bitset<32>((uint32_t)hw_res) << endl;
-
-
-    cout << bitset<32>(*(uint32_t*)&num1) << endl;
-
-    hw_num1.print();
-    hw_num2.print();
-    hw_res.print();
-
-    constexpr int FLOAT_MIN = 10;
-    constexpr int FLOAT_MAX = 100;
-
-    cout << numeric_limits<float>::min() << endl;
-    cout << numeric_limits<float>::max() << endl;
-
-    cout << endl;
-
-    std::random_device rd;
-    std::default_random_engine eng(rd());
-    std::uniform_real_distribution<float> distr(numeric_limits<float>::min(),numeric_limits<float>::max() );
-
-
-    for (int n = 0; n < 20; ++n) {
-        cout << distr(eng) << "\n";
-    }
-    
-
+    Tester t1;
+    t1.test_equal();
 
     return 0;
 }
+
+
